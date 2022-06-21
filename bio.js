1c076d00a7059868d9917a01742261d67506a60d
Abeeujah <abeeujah@gmail.com>
2022-06-21 05:16:38 +0100

    Mod

--- bio.js
+++ bio.js
@@ -2,5 +2,8 @@
 let myName = "Abraham";
 let height = "5'8";
 let country = "Nigeria";
+let bio = `Name: ${myName}, Height: ${height}, Country: ${country}.`;
 
-console.log(`Name: ${myName}, Height: ${height}, Country: ${country}.`);
+console.log(bio);
+document.write(`<h1>${bio}</h1>`)
+alert(bio)
